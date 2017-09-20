376f6bb7ed40b1c94e8b803938812defd6353d0f
KHOA LE <khoaln.vn@gmail.com>
2017-09-20 17:49:37 +0700

    update search screen

--- js/components/search/index.js
+++ js/components/search/index.js
@@ -12,8 +12,10 @@ import {
   Left,
   Right,
   Body,
-  Card,
-  CardItem
+  Card, CardItem,
+  Tab, Tabs, TabHeading,
+  ListItem, List,
+  Radio
 } from "native-base";
 import { Grid, Row, Col } from "react-native-easy-grid";
 
@@ -33,7 +35,7 @@ class Search extends Component {
     const { props: { index, list } } = this;
     return (
       <Container style={styles.container}>
-        <Header>
+        <Header hasTabs>
           <Left>
             <Button transparent onPress={() => this.props.navigation.goBack()}>
               <Icon name="ios-arrow-back" /><Text>Back</Text>
@@ -47,68 +49,254 @@ class Search extends Component {
           <Right />
         </Header>
 
-        <Content>
-          <Grid style={styles.mt}>
-            <Col>
-              <Row>
-                <Card>
-                  <CardItem cardBody>
-                    <Image source={require('../../../images/igi.png')} style={styles.image}/>
-                  </CardItem>
-                  <CardItem>
-                    <Body>
-                      <Text>
-                        0.13 Ora Diamond Certified, 0.13 Ora Diamond
-                      </Text>
-                    </Body>
-                  </CardItem>
-                  <CardItem>
-                    <Left>
-                      <Button transparent>
-                        <Icon active name="ios-information-circle-outline" />
-                      </Button>
-                    </Left>
-                    <Right>
-                      <Button transparent>
-                        <Icon active name="ios-cart" />
-                        <Text>$559</Text>
-                      </Button>
-                    </Right>
-                  </CardItem>
-                </Card>
-              </Row>
-            </Col>
-            <Col>
-              <Row>
-                <Card>
-                  <CardItem cardBody>
-                    <Image source={require('../../../images/igi.png')} style={styles.image}/>
-                  </CardItem>
-                  <CardItem>
-                    <Body>
-                      <Text>
-                        0.13 Ora Diamond Certified, 0.13 Ora Diamond
-                      </Text>
-                    </Body>
-                  </CardItem>
-                  <CardItem>
-                    <Left>
-                      <Button transparent>
-                        <Icon active name="ios-information-circle-outline" />
-                      </Button>
-                    </Left>
-                    <Right>
-                      <Button transparent>
-                        <Icon active name="ios-cart" />
-                        <Text> $559</Text>
-                      </Button>
-                    </Right>
-                  </CardItem>
-                </Card>
-              </Row>
-            </Col>
-          </Grid>
-        </Content>
+        <Tabs initialPage={2}>
+          <Tab heading={ <TabHeading><Text style={{fontSize: 16}}>Filters</Text><Icon style={{fontSize: 16}} name="ios-options" /></TabHeading>}>
+            <Content style={{opacity: 0.5}}>
+              <Grid style={styles.mt}>
+                <Col>
+                  <Row>
+                    <Card>
+                      <CardItem cardBody>
+                        <Image source={require('../../../images/igi.png')} style={styles.image}/>
+                      </CardItem>
+                      <CardItem>
+                        <Body>
+                          <Text>
+                            0.13 Ora Diamond Certified, 0.13 Ora Diamond
+                          </Text>
+                        </Body>
+                      </CardItem>
+                      <CardItem>
+                        <Left>
+                          <Button transparent>
+                            <Icon active name="ios-information-circle-outline" />
+                          </Button>
+                        </Left>
+                        <Right>
+                          <Button transparent>
+                            <Icon active name="ios-cart" />
+                            <Text> $559</Text>
+                          </Button>
+                        </Right>
+                      </CardItem>
+                    </Card>
+                  </Row>
+                </Col>
+                <Col>
+                  <Row>
+                    <Card>
+                      <CardItem cardBody>
+                        <Image source={require('../../../images/igi.png')} style={styles.image}/>
+                      </CardItem>
+                      <CardItem>
+                        <Body>
+                          <Text>
+                            0.13 Ora Diamond Certified, 0.13 Ora Diamond
+                          </Text>
+                        </Body>
+                      </CardItem>
+                      <CardItem>
+                        <Left>
+                          <Button transparent>
+                            <Icon active name="ios-information-circle-outline" />
+                          </Button>
+                        </Left>
+                        <Right>
+                          <Button transparent>
+                            <Icon active name="ios-cart" />
+                            <Text> $559</Text>
+                          </Button>
+                        </Right>
+                      </CardItem>
+                    </Card>
+                  </Row>
+                </Col>
+              </Grid>
+            </Content>
+          </Tab>
+          <Tab heading={ <TabHeading><Text style={{fontSize: 16}}>Sort by</Text><Icon style={{fontSize: 16}} name="ios-arrow-down" /></TabHeading>}>
+            <List>
+              <ListItem>
+                <Grid>
+                  <Col size={65}>
+                    <Row>
+                      <Left>
+                        <Icon style={{fontSize: 14, color: '#ffa800'}} name="arrow-round-down" />
+                        <Text style={{fontSize: 8, marginLeft: -10, color: '#ffa800'}}>HIGHEST TO LOWEST</Text>
+                      </Left>
+                    </Row>
+                  </Col>
+                  <Col size={35}>
+                    <Row>
+                      <Left>
+                        <Icon style={{fontSize: 14}} name="arrow-round-up" />
+                        <Text style={{fontSize: 8, marginLeft: -10}}>LOWEST TO HIGHEST</Text>
+                      </Left>
+                    </Row>
+                  </Col>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Text style={{fontSize: 16}}>Shape</Text>
+                <Right>
+                  <Radio selected={false} />
+                </Right>
+              </ListItem>
+              <ListItem>
+                <Text style={{fontSize: 16}}>Carat</Text>
+                <Right>
+                  <Radio selected={true} />
+                </Right>
+              </ListItem>
+              <ListItem>
+                <Text style={{fontSize: 16}}>Color</Text>
+                <Right>
+                  <Radio selected={true} />
+                </Right>
+              </ListItem>
+              <ListItem>
+                <Text style={{fontSize: 16}}>Price</Text>
+                <Right>
+                  <Radio selected={true} />
+                </Right>
+              </ListItem>
+              <Button block>
+                <Text style={{fontSize: 16}}>APPLY</Text>
+              </Button>
+            </List>
+            <Content style={{opacity: 0.5}}>
+              <Grid style={styles.mt}>
+                <Col>
+                  <Row>
+                    <Card>
+                      <CardItem cardBody>
+                        <Image source={require('../../../images/igi.png')} style={styles.image}/>
+                      </CardItem>
+                      <CardItem>
+                        <Body>
+                          <Text>
+                            0.13 Ora Diamond Certified, 0.13 Ora Diamond
+                          </Text>
+                        </Body>
+                      </CardItem>
+                      <CardItem>
+                        <Left>
+                          <Button transparent>
+                            <Icon active name="ios-information-circle-outline" />
+                          </Button>
+                        </Left>
+                        <Right>
+                          <Button transparent>
+                            <Icon active name="ios-cart" />
+                            <Text> $559</Text>
+                          </Button>
+                        </Right>
+                      </CardItem>
+                    </Card>
+                  </Row>
+                </Col>
+                <Col>
+                  <Row>
+                    <Card>
+                      <CardItem cardBody>
+                        <Image source={require('../../../images/igi.png')} style={styles.image}/>
+                      </CardItem>
+                      <CardItem>
+                        <Body>
+                          <Text>
+                            0.13 Ora Diamond Certified, 0.13 Ora Diamond
+                          </Text>
+                        </Body>
+                      </CardItem>
+                      <CardItem>
+                        <Left>
+                          <Button transparent>
+                            <Icon active name="ios-information-circle-outline" />
+                          </Button>
+                        </Left>
+                        <Right>
+                          <Button transparent>
+                            <Icon active name="ios-cart" />
+                            <Text> $559</Text>
+                          </Button>
+                        </Right>
+                      </CardItem>
+                    </Card>
+                  </Row>
+                </Col>
+              </Grid>
+            </Content>
+          </Tab>
+          <Tab heading={ 
+            <TabHeading>
+              <Button transparent style={{width: 0}}><Icon name="ios-list" style={{color: '#6b6b6b'}}/></Button>
+              <Button transparent style={{width: 0}}><Icon active style={{fontSize: 16}} name="ios-apps"/></Button>
+            </TabHeading>}>
+
+            <Content>
+              <Grid style={styles.mt}>
+                <Col>
+                  <Row>
+                    <Card>
+                      <CardItem cardBody>
+                        <Image source={require('../../../images/igi.png')} style={styles.image}/>
+                      </CardItem>
+                      <CardItem>
+                        <Body>
+                          <Text>
+                            0.13 Ora Diamond Certified, 0.13 Ora Diamond
+                          </Text>
+                        </Body>
+                      </CardItem>
+                      <CardItem>
+                        <Left>
+                          <Button transparent>
+                            <Icon active name="ios-information-circle-outline" />
+                          </Button>
+                        </Left>
+                        <Right>
+                          <Button transparent>
+                            <Icon active name="ios-cart" />
+                            <Text> $559</Text>
+                          </Button>
+                        </Right>
+                      </CardItem>
+                    </Card>
+                  </Row>
+                </Col>
+                <Col>
+                  <Row>
+                    <Card>
+                      <CardItem cardBody>
+                        <Image source={require('../../../images/igi.png')} style={styles.image}/>
+                      </CardItem>
+                      <CardItem>
+                        <Body>
+                          <Text>
+                            0.13 Ora Diamond Certified, 0.13 Ora Diamond
+                          </Text>
+                        </Body>
+                      </CardItem>
+                      <CardItem>
+                        <Left>
+                          <Button transparent>
+                            <Icon active name="ios-information-circle-outline" />
+                          </Button>
+                        </Left>
+                        <Right>
+                          <Button transparent>
+                            <Icon active name="ios-cart" />
+                            <Text> $559</Text>
+                          </Button>
+                        </Right>
+                      </CardItem>
+                    </Card>
+                  </Row>
+                </Col>
+              </Grid>
+            </Content>
+          </Tab>
+        </Tabs>
       </Container>
     );
   }
